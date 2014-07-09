aad1aa2d8d393d27136d3a26fae15b1bbbe34b77
IjzerenHein <hrutjes@gmail.com>
2014-07-09 15:56:41 +0200

    Updated samples to work with v0.2.2 build of famo.us

--- basics-step4/client/basics.js
+++ basics-step4/client/basics.js
@@ -1,35 +1,33 @@
 // 4. ScrollView
 Famous.main(function(require) {
-  var Engine = require('famous/engine');
-  var Surface = require('famous/surface');
-  var Modifier = require('famous/modifier');
-  var Matrix = require('famous/transform');
-  var ScrollView = require('famous/views/scrollview');
+    var Engine = require('famous/core/Engine');
+    var Surface = require('famous/core/Surface');
+    var ScrollView = require('famous/views/Scrollview');
 
-  var mainCtx = Engine.createContext();
+    var mainCtx = Engine.createContext();
 
-  // Create a scrollview and array to hold surfaces
-  var scrollView = new ScrollView();
-  var surfaces = [];
-  
-  // Create a numbered surface
-  function createSurface(number) {
-    return new Surface({
-      size: [undefined, 100],
-      content: "<p>Surface " + number + "</p>",
-      classes: ["test-surface", "n" + i]
-    });
-  }
+    // Create a scrollview and array to hold surfaces
+    var scrollView = new ScrollView();
+    var surfaces = [];
 
-  // Add many surfaces to the scrollView
-  for (var i = 0;i < 20;i++) {
-    surfaces.push(createSurface(i));
-  }
+    // Create a numbered surface
+    function createSurface(number) {
+        return new Surface({
+            size: [undefined, 100],
+            content: '<p>Surface ' + number + '</p>',
+            classes: ['test-surface', 'n' + i]
+        });
+    }
 
-  // Include the surfaces in the scrollview and pipe
-  // events to it from the engine
-  scrollView.sequenceFrom(surfaces);
-  Engine.pipe(scrollView);
+    // Add many surfaces to the scrollView
+    for (var i = 0;i < 20;i++) {
+        surfaces.push(createSurface(i));
+    }
 
-  mainCtx.link(scrollView);
+    // Include the surfaces in the scrollview and pipe
+    // events to it from the engine
+    scrollView.sequenceFrom(surfaces);
+    Engine.pipe(scrollView);
+
+    mainCtx.add(scrollView);
 });
--- basics-step6/client/basics.js
+++ basics-step6/client/basics.js
@@ -1,55 +1,54 @@
 // 6. Full leaderboard
 Famous.main(function(require) {
-  var Engine = require('famous/engine');
-  var Surface = require('famous/surface');
-  var Modifier = require('famous/modifier');
-  var Matrix = require('famous/transform');
-  var ScrollView = require('famous/views/scrollview');
-  var HeaderFooterLayout = require('famous/views/header-footer-layout')
-  
-  var mainCtx = Engine.createContext();
-  
-  // Create a 3 horitzontal paned layout
-  var layout = new HeaderFooterLayout({
-    headerSize: 0,
-    footerSize: 50
-  });
+    var Engine = require('famous/core/Engine');
+    var Surface = require('famous/core/Surface');
+    var ScrollView = require('famous/views/Scrollview');
+    var HeaderFooterLayout = require('famous/views/HeaderFooterLayout')
 
-  // Create a scrollview and array to hold surfaces
-  var scrollView = new ScrollView();
-  var surfaces = [];
+    var mainCtx = Engine.createContext();
 
-  // Create a surface based on data in document
-  function createSurface(doc) {
-    return new Surface({
-      size: [undefined, 50],
-      content: Meteor.render(function() {
-        return Template.player(Players.findOne(doc._id));
-      }),
-      classes: ["test-surface"]
+    // Create a 3 horitzontal paned layout
+    var layout = new HeaderFooterLayout({
+        headerSize: 0,
+        footerSize: 50
     });
-  }
-  
-  // Re-actively maintain the surfaces array as players change.
-  cursorToArray(
-    Players.find({}, {sort: {score: -1, name: 1}}),
-    surfaces,
-    createSurface
-  );
 
-  // Include the surfaces in the scrollview and pipe
-  // events to it from the engine
-  scrollView.sequenceFrom(surfaces);
-  Engine.pipe(scrollView);
+    // Create a scrollview and array to hold surfaces
+    var scrollView = new ScrollView();
+    var surfaces = [];
 
-  // Link the scrollview to the layout and add the footer
-  layout.id.content.link(scrollView);
-  layout.id.footer.link(new Surface({
-    size: [undefined, 50],
-    content: Meteor.render(function() { return Template.leaderboard()}),
-    classes: ['footer']
-  }));
+    // Create a surface based on data in document
+    function createSurface(doc) {
+        return new Surface({
+            size: [undefined, 50],
+            content: Meteor.render(function() {
+                return Template.player(Players.findOne(doc._id));
+            }),
+            classes: ['test-surface']
+        });
+    }
 
-  mainCtx.link(layout);
+    // Re-actively maintain the surfaces array as players change.
+    cursorToArray(
+        Players.find({}, {sort: {score: -1, name: 1}}),
+        surfaces,
+        createSurface
+    );
+
+    // Include the surfaces in the scrollview and pipe
+    // events to it from the engine
+    scrollView.sequenceFrom(surfaces);
+    Engine.pipe(scrollView);
+
+    // Link the scrollview to the layout and add the footer
+    layout.content.add(scrollView);
+    layout.footer.add(new Surface({
+        size: [undefined, 50],
+        content: Meteor.render(function() {
+            return Template.leaderboard()
+        }),
+        classes: ['footer']
+    }));
+
+    mainCtx.add(layout);
 });
-
